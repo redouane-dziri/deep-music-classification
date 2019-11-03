a371ecbdeac898acb89a82ee95e15b45913ab305
Redouane Dziri <rd2853@columbia.edu>
2019-11-03 17:51:02 -0500

    fetch data and early pre-processing

---
+++ early_preprocessing/sample.py
@@ -0,0 +1,52 @@
+import os
+import shutil
+
+import json
+
+import numpy as np
+import pandas as pd
+
+from git_root import git_root
+
+with open(git_root("config", "config.json"), 'r') as config:
+    config = json.load(config)
+
+data_root = git_root("data")
+
+train_test_split = pd.read_csv(
+    os.path.join(data_root, "metadata", "train_test_split.csv")
+)
+
+def sample_8_2(block):
+    sample_size = 2
+    if block["split"].unique()[0] == "train":
+        sample_size = 8
+    return block.sample(sample_size)
+
+samples = train_test_split.groupby(["genre", "split"]).\
+    apply(sample_8_2).\
+    reset_index(drop=True)
+
+sample_data_path = git_root("data", "sample_data")
+full_data_path = git_root("data", "full_data")
+
+def make_dir(path):
+    if not os.path.isdir(path):
+        os.mkdir(path)
+
+for split in ["train", "test"]:
+    make_dir(os.path.join(sample_data_path, split))
+    for genre in config["genres"]:
+        make_dir(os.path.join(sample_data_path, split, genre))
+
+def copy_file(row):
+    shutil.copy(
+        os.path.join(
+            full_data_path, row["split"], row["genre"], row["file_name"]
+        ), 
+        os.path.join(
+            sample_data_path, row["split"], row["genre"], row["file_name"]
+        )
+    )
+
+_ = samples.apply(copy_file, axis=1)
