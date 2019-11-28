cf6ba53e30e810de2520cd00c32d0f713238a17d
arnaudstiegler <arnaud.stiegler@hotmail.fr>
2019-11-27 20:14:20 -0500

    fixing model save issue

--- models/affine_scalar_layer.py
+++ models/affine_scalar_layer.py
@@ -3,24 +3,26 @@ from tensorflow.keras import layers
 
 
 class AffineScalar(layers.Layer):
-    """[summary]
-    
-    Arguments:
-        layers {[type]} -- [description]
-    """
+	"""[summary]
+	
+	Arguments:
+		layers {[type]} -- [description]
+	"""
 
-    def __init__(self, **kwargs):
-        super(AffineScalar, self).__init__(**kwargs)
-        self.w = self.add_weight(
-            shape=(1,),
-            initializer="random_normal",
-            trainable=True
-        )
-        self.b = self.add_weight(
-            shape=(1,),
-            initializer="zeros",
-            trainable=True
-        )
-
-    def call(self, inputs):
-        return self.w * inputs + self.b
+	def __init__(self, **kwargs):
+		super(AffineScalar, self).__init__(**kwargs)
+		self.w = self.add_weight(
+			shape=(1,),
+			initializer="random_normal",
+			trainable=True,
+			name='affineScalar_w'
+		)
+		self.b = self.add_weight(
+			shape=(1,),
+			initializer="zeros",
+			trainable=True,
+			name='affineScalar_b'
+		)
+		
+	def call(self, inputs):
+		return self.w * inputs + self.b
