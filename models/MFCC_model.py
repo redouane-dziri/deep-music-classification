14a3e4914ad8c087416a8d82138c12753cc4535f
arnaudstiegler <arnaud.stiegler@gmail.com>
2019-11-29 16:31:23 -0500

    updated mfcc process

--- models/MFCC_model.py
+++ models/MFCC_model.py
@@ -23,8 +23,9 @@ def setup_model():
     config = load_config()
     params = load_params()
     input_dim_1 = params["MFCC"]["n_mfcc"]
-    input_dim_3 = params["MFCC"]["n_submaps"]
-    input_dim_2 = params["MFCC"]["n_windows"] // input_dim_3
+    #input_dim_3 = params["MFCC"]["n_submaps"]
+    input_dim_3 = 1 #piece-wise model (there are 30 submaps per track)
+    input_dim_2 = params["MFCC"]["n_windows"] // params["MFCC"]["n_submaps"] #time length of a submap
     n_genres = len(config["genres"])
 
     model = Sequential()
@@ -37,10 +38,12 @@ def setup_model():
             name="conv1"
         )
     )
-    model.add(AveragePooling2D((2, 2), name="pooling1"))
+    model.add(AveragePooling2D((4, 4), name="pooling1"))
     model.add(AffineScalar(name="affine"))
     model.add(Conv2D(12, (3,3), activation="tanh", name="conv2"))
     model.add(GlobalAveragePooling2D(name="pooling2"))
+    #model.add(AveragePooling2D((4, 4), name="pooling1"))
+    #model.add(AffineScalar(name="affine"))
     model.add(Dense(n_genres, activation="softmax", name="dense"))
 
     return model
