f371a2c1841bf39471922f215f8ad1127b58953c
Redouane Dziri <rd2853@columbia.edu>
2019-12-08 17:10:35 -0500

    new concatenated model

--- models/affine_scalar_layer.py
+++ models/affine_scalar_layer.py
@@ -3,26 +3,34 @@ from tensorflow.keras import layers
 
 
 class AffineScalar(layers.Layer):
-	"""[summary]
-	
-	Arguments:
-		layers {[type]} -- [description]
+	"""This layer multiplies its input by a trainable weight and adds a 
+	trainable bias
 	"""
 
-	def __init__(self, **kwargs):
+	# `suffix` is used for saving purposes to uniquely identify saved weights
+	def __init__(self, suffix=0, **kwargs):
 		super(AffineScalar, self).__init__(**kwargs)
 		self.w = self.add_weight(
 			shape=(1,),
 			initializer="random_normal",
 			trainable=True,
-			name='affineScalar_w'
+			name=f'affineScalar_w{suffix}'
 		)
 		self.b = self.add_weight(
 			shape=(1,),
 			initializer="zeros",
 			trainable=True,
-			name='affineScalar_b'
+			name=f'affineScalar_b{suffix}'
 		)
+		self.suffix = suffix
+
+	def get_config(self):
+		
+		config = super().get_config().copy()
+		config.update({
+            'suffix': self.suffix,
+        })
+		return config
 		
 	def call(self, inputs):
 		return self.w * inputs + self.b
